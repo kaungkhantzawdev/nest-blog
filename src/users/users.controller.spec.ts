edf80b2e51fb442e7d65b31c8401a987a6ce9254
kaungkhantzawjudgify <kaung@judgify.me>
2023-09-27 15:58:47 +0630

    add testing to auth

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthGuard } from '../guards/auth.guard';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
+    const fakeUserService: Partial<UsersService> = {
+      findAll: () => Promise.resolve([]),
+    };
+
+    const fakeAuthGuard = { canActivate: jest.fn(() => true) };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-    }).compile();
+      providers: [
+        {
+          provide: UsersService,
+          useValue: fakeUserService,
+        },
+      ],
+    })
+      .overrideGuard(AuthGuard)
+      .useValue(fakeAuthGuard)
+      .compile();
 
     controller = module.get<UsersController>(UsersController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('find all users', async () => {
+    const users = await controller.findAllUsers();
+    expect(users.entries).toBeDefined;
   });
 });
